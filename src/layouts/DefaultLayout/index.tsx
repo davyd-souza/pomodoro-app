1008ca3b48aba587fbb44c75a10ca1d8e15c81c0
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-26 15:28:10 -0300

    feat: implement style to default layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -4,11 +4,14 @@ import { Outlet } from 'react-router-dom'
 // COMPONENT
 import { Header } from '../components/Header'
 
+// STYLES
+import { LayoutContainer } from './styles'
+
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,7 @@
+// DEPENDENCY
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  display: grid;
+  gap: 4rem;
+`
