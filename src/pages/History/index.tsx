0d4ffae0354e330bf719439055f31df78baf8448
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-26 22:47:53 -0300

    feat: implement History html structure

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,46 @@
+export function History() {
+  return (
+    <main>
+      <h2>My history</h2>
+
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>In Progress</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>topped</td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>Finished</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </main>
+  )
+}
