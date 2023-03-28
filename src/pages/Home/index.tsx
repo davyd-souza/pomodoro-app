e58d4b97896c24aa86251769aa9c8a788cea89c9
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-27 23:48:20 -0300

    feat: implement catch form data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,8 @@
+// DEPENDENCY
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
 // STYLE
 import {
   ButtonStyled,
@@ -9,16 +14,45 @@ import {
 } from './styles'
 import { Play } from 'phosphor-react'
 
+const newCountdownFormValidationSchema = z.object({
+  task: z.string().min(1, 'Please type your task'),
+  minutesAmount: z.number().min(5).max(60),
+})
+
+type NewCycleFormData = z.infer<typeof newCountdownFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCountdownFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  const handleCreateNewCountdown = (data: NewCycleFormData) => {
+    console.log(data)
+
+    // Will reset value to defaultValues inside of useForm hook
+    reset()
+  }
+
   return (
     <HomeContainer>
-      <FormContainer id="timer">
+      <FormContainer
+        id="timer"
+        onSubmit={handleSubmit(handleCreateNewCountdown)}
+      >
         <label htmlFor="task">I&apos;m going to work on</label>
         <InputStyled
           type="text"
           id="task"
           placeholder="Give your task a name"
           autoComplete="on"
+          {...register('task')}
         />
 
         <label htmlFor="minutesAmount">for</label>
@@ -29,6 +63,7 @@ export function Home() {
           step={5}
           max={60}
           min={5}
+          {...register('minutesAmount', { valueAsNumber: true })}
         />
 
         <span>minutes.</span>
@@ -42,7 +77,7 @@ export function Home() {
         <span>0</span>
       </CountdownContainer>
 
-      <ButtonStyled form="timer" type="submit">
+      <ButtonStyled form="timer" type="submit" disabled={isSubmitDisabled}>
         Start
         <Play size={24} weight="bold" />
       </ButtonStyled>
